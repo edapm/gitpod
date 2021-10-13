df7dacb2daa835a72014799534df1cdccb0cbd88
edapm <mynameisprivate3@gmail.com>
2021-10-13 20:30:07 +0000

    Svelte course (until 47:47)

---
+++ svelte-feedback-app/src/components/Card.svelte
@@ -0,0 +1,16 @@
+<main>
+    <div class="card">
+        <slot></slot>
+    </div>
+</main>
+
+<style>
+    .card {
+        background-color: #fff;
+        color: #333;
+        border-radius: 15px;
+        padding: 40px 50px;
+        margin: 20px 0;
+        position: relative;
+    }
+</style>
